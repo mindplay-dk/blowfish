efe1488545d3af584aa291085ad9f1ef888844d4
Rasmus Schultz <rasmus@mindplay.dk>
2014-11-17 22:44:25 +0100

    remove redundant autoloader config

--- test.php
+++ test.php
@@ -1,8 +1,6 @@
 <?php
 
-/** @var \Composer\Autoload\ClassLoader $autoloader */
-$autoloader = require __DIR__ . '/vendor/autoload.php';
-$autoloader->addPsr4('mindplay\blowfish\\', __DIR__ . '/src');
+require __DIR__ . '/vendor/autoload.php';
 
 use mindplay\blowfish\BlowfishService;
 
